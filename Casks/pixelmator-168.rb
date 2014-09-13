d7a31f2a974083bfda57a55369d162628b5398b8
Marshall Mickelson <marshallmick+git@gmail.com>
2014-09-13 15:59:39 -0700

    initial version

---
+++ Casks/pixelmator-168.rb
@@ -0,0 +1,9 @@
+class Pixelmator168 < Cask
+  version '1.6.8'
+  sha256 '328acf75345e2379a1235bf918f6a76ecfa6095be4d27a7aabfd8baab04f7e48'
+
+  url 'https://dl.dropboxusercontent.com/s/24nf8hqvb1jfqll/Pixelmator-168.zip'
+  homepage 'http://pixelmator.com'
+
+  link 'Pixelmator-168.app'
+end

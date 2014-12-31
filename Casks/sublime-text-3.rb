b6dbc0f2343f85023d75a3decfb41baecbd2f7c4
Marshall Mickelson <marshallmick+git@gmail.com>
2014-12-30 20:13:17 -0800

    Added Sublime Text 3 Beta

---
+++ Casks/sublime-text-3.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'sublime-text3' do
+  version 'Build 3065'
+  sha256 '373dbfcddab7fb288351763402361cd4045f4a9510eb32f81938d0c156d161c3'
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg'
+  appcast 'http://www.sublimetext.com/updates/3/stable/appcast_osx.xml',
+    :sha256 => 'bb8df68fe6a87966c4662948f4f147ac5dd0d1cd3a8d5342c56a11a018ff81e9'
+  homepage 'http://www.sublimetext.com/3'
+  license :closed
+  app 'Sublime Text.app'
+  binary 'Sublime Text.app/Contents/SharedSupport/bin/subl'
+  caveats do
+    files_in_usr_local
+  end
+end
